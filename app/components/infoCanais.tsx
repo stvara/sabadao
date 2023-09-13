57df82d8c430b95ab0c48db95d36f23dbe445e41
stvara <svara011@gmail.com>
2023-09-12 23:39:45 -0300

    Lista de canais

---
+++ app/components/infoCanais.tsx
@@ -0,0 +1,20 @@
+﻿import React from "react";
+
+const InfoCanais = ({nome,conteudo,tema,img,link}) => {
+    return (
+        <div className='flex'>
+            <div className='w-1/2'>
+                <img className='rounded-3xl' src={img} alt="" />
+            </div>
+            <div className='w-1/2'>
+                <h1>Nome: {nome}</h1>
+                <h2>Conteúdo: {conteudo}</h2>
+                <h3>Tema: {tema}</h3>
+                <a href={link} target="_blank">Canal</a>
+            </div>
+
+        </div>
+    );
+};
+
+export default InfoCanais;
